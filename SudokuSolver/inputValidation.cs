6bdf8e1a173eb30670cb591da2ce64a278e4bb46
amitbarak <amitbarak2004@gmail.com>
2022-12-23 15:07:09 +0200

    validation added

---
+++ SudokuSolver/inputValidation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    static class InputValidation
+    {
+        public static Boolean isValid(String input)
+        {
+            Boolean valid = checkLegth(input);
+            valid &= checkChars(input);
+            return valid;
+        }
+
+        public static Boolean checkChars(String input)
+        {
+            char max_value = GeneralValues.max_char;
+            char min_value = GeneralValues.min_char;
+            Console.WriteLine("hello");
+            foreach (char c in input)
+            {
+                Console.WriteLine(c + min_value + max_value);
+                if ( c > max_value || c < min_value)
+                {
+                    Console.WriteLine("hi");
+                    GeneralValues.error_message = "wrong char: " + c;
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static Boolean checkLegth(String input)
+        {
+            if (input.Length == GeneralValues.Size * GeneralValues.Size)
+            {
+                return true;
+            }
+            else
+            {
+                GeneralValues.error_message = "wrong size";
+                return false;
+            }
+        }
+
+    }
+}
